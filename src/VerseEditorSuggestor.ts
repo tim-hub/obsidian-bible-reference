78270145b19f62ca2c0dd6025ab275f375364f6c
tim-hub <tim-hub@users.noreply.github.com>
2023-02-16 16:01:46 +1300

    keep EditorSuggester to keep old path (for git history purpose)

--- src/VerseEditorSuggestor.ts
+++ src/VerseEditorSuggestor.ts
@@ -1,8 +1,16 @@
-import { Editor, EditorPosition, EditorSuggest, EditorSuggestContext, EditorSuggestTriggerInfo, TFile } from 'obsidian';
-import BibleReferencePlugin from './main';
-import { VerseTypoCheck } from './VerseTypoCheck';
-import { VerseSuggesting } from './VerseSuggesting';
-import { BibleReferencePluginSettings } from './data/constants';
+import {
+  Editor,
+  EditorPosition,
+  EditorSuggest,
+  EditorSuggestContext,
+  EditorSuggestTriggerInfo,
+  TFile,
+} from "obsidian";
+import BibleReferencePlugin from "./main";
+import { VerseTypoCheck } from "./VerseTypoCheck";
+import { VerseSuggesting } from "./VerseSuggesting";
+import { BibleReferencePluginSettings } from "./data/constants";
+import { getSuggestionsFromQuery } from "./suggesetor/VerseSuggestor";
 
 /**
  * Extend the EditorSuggest to suggest bible verses.
@@ -11,7 +19,10 @@ export class VerseEditorSuggestor extends EditorSuggest<VerseSuggesting> {
   plugin: BibleReferencePlugin;
   settings: BibleReferencePluginSettings;
 
-  constructor(plugin: BibleReferencePlugin, settings: BibleReferencePluginSettings) {
+  constructor(
+    plugin: BibleReferencePlugin,
+    settings: BibleReferencePluginSettings
+  ) {
     super(plugin.app);
     this.plugin = plugin;
     this.settings = settings;
@@ -23,11 +34,15 @@ export class VerseEditorSuggestor extends EditorSuggest<VerseSuggesting> {
    * @param editor
    * @param _
    */
-  onTrigger(cursor: EditorPosition, editor: Editor, _: TFile): EditorSuggestTriggerInfo | null {
+  onTrigger(
+    cursor: EditorPosition,
+    editor: Editor,
+    _: TFile
+  ): EditorSuggestTriggerInfo | null {
     const currentContent = editor.getLine(cursor.line).substring(0, cursor.ch);
     const match = VerseTypoCheck(currentContent);
     if (match) {
-      console.debug('trigger on', currentContent);
+      console.debug("trigger on", currentContent);
       return {
         end: cursor,
         start: {
@@ -44,38 +59,18 @@ export class VerseEditorSuggestor extends EditorSuggest<VerseSuggesting> {
    * Suggest bible verses.
    * @param context
    */
-  async getSuggestions(context: EditorSuggestContext): Promise<VerseSuggesting[]> {
-    console.debug('get suggestion for query ', context.query.toLowerCase());
-
-    const bookName = context.query.match(/[123]*[A-z]{3,}/)?.first();
-
-    if (!bookName) {
-      console.error(`could not find through query`, context.query);
-      return [];
-    }
-
-    const numbersPartsOfQueryString = context.query.substring(2 + bookName.length);
-    const numbers = numbersPartsOfQueryString.split(/[-:]+/);
-
-    const chapterNumber = parseInt(numbers[0]);
-    const verseNumber = parseInt(numbers[1]);
-    const verseEndNumber = numbers.length === 3 ? parseInt(numbers[2]) : undefined;
-
-    // todo get bibleVersion and language from settings
-    const suggestingVerse = new VerseSuggesting(this.settings, bookName, chapterNumber, verseNumber, verseEndNumber);
-
-    console.debug(bookName, chapterNumber, verseNumber, verseEndNumber, suggestingVerse, this.settings);
-    await suggestingVerse.fetchAndSetVersesText();
-    return [suggestingVerse];
+  async getSuggestions(
+    context: EditorSuggestContext
+  ): Promise<VerseSuggesting[]> {
+    return getSuggestionsFromQuery(context.query, this.settings);
   }
 
   renderSuggestion(suggestion: VerseSuggesting, el: HTMLElement): void {
-    const outer = el.createDiv({ cls: "obr-suggester-container" });
-    outer.createDiv({ cls: "obr-shortcode" }).setText(suggestion.text);
+    suggestion.renderSuggestion(el);
   }
 
   selectSuggestion(suggestion: VerseSuggesting): void {
-    if(this.context) {
+    if (this.context) {
       (this.context.editor as Editor).replaceRange(
         suggestion.ReplacementContent,
         this.context.start,
