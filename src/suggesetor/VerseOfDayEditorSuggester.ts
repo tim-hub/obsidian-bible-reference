92b5810433398331ea0e1c6bea3998177df8d43c
tim-hub <tim-hub@users.noreply.github.com>
2023-08-27 16:46:31 +1200

    finish a basic vod

--- src/suggesetor/VerseOfDayEditorSuggester.ts
+++ src/suggesetor/VerseOfDayEditorSuggester.ts
@@ -1,9 +1,12 @@
 import { Editor, EditorPosition, EditorSuggest, EditorSuggestContext, EditorSuggestTriggerInfo, TFile } from 'obsidian';
 import BibleReferencePlugin from '../main';
 import { BibleReferencePluginSettings } from '../data/constants';
+import { getVod, VerseOfDayResponse } from '../provider/VODProvider';
 
 
 export class VerseOfDayEditorSuggester extends EditorSuggest<string> {
+  private plugin: BibleReferencePlugin;
+  private settings: BibleReferencePluginSettings;
 
   constructor(
     plugin: BibleReferencePlugin,
@@ -14,14 +17,15 @@ export class VerseOfDayEditorSuggester extends EditorSuggest<string> {
     this.settings = settings
   }
 
-
-  getSuggestions(context: EditorSuggestContext): string[] | Promise<string[]> {
-    return ['God is Love'];
+  async getSuggestions(context: EditorSuggestContext): Promise<string[]> {
+    const vodResp = await getVod();
+    return [this.buildSuggestion(vodResp)];
   }
 
   /**
    * This will build the EditorSuggestContext in getSuggestions
    * This is monitoring when --vod got typed.
+   * Make sure the trigger not conflicts with other suggesters.
    * @param cursor
    * @param editor
    * @param file
@@ -55,6 +59,12 @@ export class VerseOfDayEditorSuggester extends EditorSuggest<string> {
         this.context.end
       )
     }
+  }
+
+  private buildSuggestion(vod: VerseOfDayResponse): string {
+    return `> [!Bible] ${vod.verse.details.reference}
+> ${vod.verse.details.text}
+    `
   }
 
 }
