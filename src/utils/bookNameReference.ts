5e6d9fbeeb6d3fd49dc31ac80b1dada8513a0419
tim-hub <tim-hub@users.noreply.github.com>
2023-11-25 21:33:02 +1300

    2.3.6 fix Spainish short name trigger
    fix https://github.com/tim-hub/obsidian-bible-reference/issues/149

--- src/utils/bookNameReference.ts
+++ src/utils/bookNameReference.ts
@@ -2,24 +2,26 @@ import Reference from 'bible-reference-toolkit'
 
 export const getBookIdFromBookName = (
   bookName: string,
-  langaugeCode: string = 'en'
+  languageCode: string = 'en'
 ): number => {
   try {
-    return Reference.bookIdFromTranslationAndName(langaugeCode, bookName)
+    console.debug('get book id first time', bookName, languageCode)
+    return Reference.bookIdFromTranslationAndName(languageCode, bookName)
   } catch (e) {
     // try in slow but in all supported languages
+    console.debug('get book id from all translations', bookName)
     return Reference.bookIdFromName(bookName)
   }
 }
 
 export const getFullBookName = (
   name: string,
-  langaugeCode: string = 'en'
+  languageCode: string = 'en'
 ): string => {
-  console.debug('getFullBookName', name, langaugeCode)
-  const bookId = getBookIdFromBookName(name, langaugeCode)
+  console.debug('getFullBookName', name, languageCode)
+  const bookId = getBookIdFromBookName(name, languageCode)
   try {
-    return Reference.bookNameFromTranslationAndId(langaugeCode, bookId)
+    return Reference.bookNameFromTranslationAndId(languageCode, bookId)
   } catch (e) {
     return Reference.bookEnglishFullNameFromId(bookId)
   }
