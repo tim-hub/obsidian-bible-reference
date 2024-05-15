6dcc71ff047717a51d2bf8171254edee7c3079c1
tim-hub <tim-hub@users.noreply.github.com>
2024-05-16 10:00:50 +1200

    allow ++ as trigger

--- src/utils/regs.ts
+++ src/utils/regs.ts
@@ -1,25 +1,17 @@
 /**
- * regular expression to match verseNumber
- *
- * -- John 1:1
- * -- Joh 1
- * -- Joh 123:1-3
- * -- 1 Col 1:1-15
- * eslint-disable-line /
-export const ORIGINAL_REG = /-{2}(([123])*)(\w{3,})\+\d{0,3}:*\d{0,3}-*\d{0,3}$/
-
- /**
- * regular expression to match
- * --John12:1-3
- * --John1:1
- */
-export const SHORT_REG = /-{2}([123])*\s*[A-z]{2,}\s*\d{1,3}:\d{1,3}(-\d{1,3})*/
-
-/**
  * regular expression to match
  * John12:1-3
  * John1:1
+ * 1John1:1
  */
 export const MODAL_REG = /([123])*\s*[A-z]{2,}\s*\d{1,3}:\d{1,3}(-\d{1,3})*/
 
 export const BOOK_REG = /[123]*\s*[A-z]{2,}/
+
+
+/**
+ * prefix of the trigger
+ * --
+ * ++
+ */
+export const DEFAULT_TRIGGER_PREFIX_REG = /--|(\+\+)/
--- src/utils/verseMatch.test.ts
+++ src/utils/verseMatch.test.ts
@@ -1,4 +1,4 @@
-import { verseMatch } from './verseMatch'
+import { verseMatch, matchTriggerPrefix } from './verseMatch'
 
 test('test verse match', () => {
   const result = verseMatch('--John3:16')
@@ -11,6 +11,28 @@ test('test verse not match', () => {
 })
 
 test('test verse not match', () => {
-  const result = verseMatch('John3:16', true)
+  const result = verseMatch('John3:16')
   expect(result).toBe('John3:16')
+})
+
+
+test('test trigger prefix', () => {
+  const result = matchTriggerPrefix('--')
+  expect(result).toBe(true)
+})
+
+test('test trigger prefix ++', () => {
+  const result = matchTriggerPrefix('++')
+  expect(result).toBe(true)
+})
+
+
+test('test trigger prefix **', () => {
+  const result = matchTriggerPrefix('**')
+  expect(result).toBe(false)
+})
+
+test('test trigger prefix ++1', () => {
+  const result = matchTriggerPrefix('**1')
+  expect(result).toBe(false)
 })
