2de4a99f292e93e23c6483350e08c617ff81feb6
tim-hub <tim-hub@users.noreply.github.com>
2024-06-22 11:50:22 +1200

    add NLT and reorder translations

--- src/data/BibleVersionCollection.ts
+++ src/data/BibleVersionCollection.ts
@@ -1,26 +1,21 @@
 import { IBibleVersion } from '../interfaces/IBibleVersion'
 import { BibleAPISourceCollection } from './BibleApiSourceCollection'
 
-/**
- * Please note to support the abbreviation of the translation
- * "code" need to be added
- * And the Abbreviation source need to be updated too See this repo. https://github.com/AntiochTechnology/bible-reference-toolkit/tree/master/packages/bible-book-names-intl
- */
-export const BibleVersionCollection: IBibleVersion[] = [
-  // bible api
+
+export const BibleVersionCollectionEnglish = [
   {
-    key: 'web',
-    versionName: 'World English Bible',
+    key: 'bbe',
+    versionName: 'Bible in Basic English',
     language: 'English',
     code: 'en',
     apiSource: BibleAPISourceCollection.bibleApi,
   },
   {
-    key: 'clementine',
-    versionName: 'Clementine Latin Vulgate',
-    language: 'Latin',
-    code: 'la',
-    apiSource: BibleAPISourceCollection.bibleApi,
+    key: 'esv',
+    versionName: 'English Standard Version',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
     key: 'kjv',
@@ -30,11 +25,55 @@ export const BibleVersionCollection: IBibleVersion[] = [
     apiSource: BibleAPISourceCollection.bibleApi,
   },
   {
-    key: 'bbe',
-    versionName: 'Bible in Basic English',
+    key: 'lsb',
+    versionName: 'Legacy Standard Bible',
     language: 'English',
     code: 'en',
-    apiSource: BibleAPISourceCollection.bibleApi,
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'mev',
+    versionName: 'Modern English Version',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
+    infoUrl: 'https://wikipedia.org/wiki/Modern_English_Version',
+  },
+  {
+    key: 'nasb',
+    versionName: 'New American Standard Bible (1995)',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'niv',
+    versionName: 'New International Version, 1984',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'nkjv',
+    versionName: 'New King James Version',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
+    infoUrl: 'https://wikipedia.org/wiki/New_King_James_Version',
+  },
+  {
+    key: 'nlt',
+    versionName: 'New Living Translation (NLT), 2015',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'nrsvce',
+    versionName: 'New Revised Standard Version Catholic Edition (NRSVCE)',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
     key: 'oeb-us',
@@ -44,12 +83,35 @@ export const BibleVersionCollection: IBibleVersion[] = [
     apiSource: BibleAPISourceCollection.bibleApi,
   },
   {
+    key: 'web',
+    versionName: 'World English Bible',
+    language: 'English',
+    code: 'en',
+    apiSource: BibleAPISourceCollection.bibleApi,
+  },
+]
+
+export const BibleVersionCollectionLatin = [
+  {
+    key: 'clementine',
+    versionName: 'Clementine Latin Vulgate',
+    language: 'Latin',
+    code: 'la',
+    apiSource: BibleAPISourceCollection.bibleApi,
+  },
+]
+
+export const BibleVersionCollectionPortuguese = [
+  {
     key: 'almeida',
     versionName: 'João Ferreira de Almeida',
     language: 'Portuguese',
     code: 'pt',
     apiSource: BibleAPISourceCollection.bibleApi,
   },
+]
+
+export const BibleVersionCollectionRomanian = [
   {
     key: 'rccv',
     versionName: 'Romanian Corrected Cornilescu Version',
@@ -57,6 +119,9 @@ export const BibleVersionCollection: IBibleVersion[] = [
     code: 'ro',
     apiSource: BibleAPISourceCollection.bibleApi,
   },
+]
+
+export const BibleVersionCollectionCherokee = [
   {
     key: 'cherokee',
     versionName: 'Cherokee New Testament',
@@ -64,6 +129,9 @@ export const BibleVersionCollection: IBibleVersion[] = [
     code: 'cu',
     apiSource: BibleAPISourceCollection.bibleApi,
   },
+]
+
+export const BibleVersionCollectionEnglishUK = [
   {
     key: 'oeb-cw',
     versionName: 'Open English Bible, Commonwealth Edition',
@@ -78,58 +146,9 @@ export const BibleVersionCollection: IBibleVersion[] = [
     code: 'en',
     apiSource: BibleAPISourceCollection.bibleApi,
   },
-  // bolls life
-  {
-    key: 'mev',
-    versionName: 'Modern English Version',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-    infoUrl: 'https://wikipedia.org/wiki/Modern_English_Version',
-  },
-  {
-    key: 'nkjv',
-    versionName: 'New King James Version',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-    infoUrl: 'https://wikipedia.org/wiki/New_King_James_Version',
-  },
-  {
-    key: 'niv',
-    versionName: 'New International Version, 1984',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'nrsvce',
-    versionName: 'New Revised Standard Version Catholic Edition (NRSVCE)',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'esv',
-    versionName: 'English Standard Version',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'nasb',
-    versionName: 'New American Standard Bible (1995)',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'lsb',
-    versionName: 'Legacy Standard Bible',
-    language: 'English',
-    code: 'en',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
+]
+
+export const BibleVersionCollectionGerman = [
   {
     key: 'elb',
     versionName: 'Elberfelder Bibel (1871)',
@@ -145,17 +164,26 @@ export const BibleVersionCollection: IBibleVersion[] = [
     apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
+    key: 'mb',
+    versionName: 'Menge-Bibel',
+    language: 'German Deutsch',
+    code: 'de',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
     key: 'sch',
     versionName: 'Schlachter (1951)',
     language: 'German Deutsch',
     code: 'de',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionGreek = [
   {
-    key: 'mb',
-    versionName: 'Menge-Bibel',
-    language: 'German Deutsch',
-    code: 'de',
+    key: 'lxx',
+    versionName: 'Septuagint',
+    language: 'Greek Ελληνικά',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
@@ -170,10 +198,15 @@ export const BibleVersionCollection: IBibleVersion[] = [
     language: 'Greek Ελληνικά',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+
+]
+
+export const BibleVersionCollectionHebrew = [
+
   {
-    key: 'lxx',
-    versionName: 'Septuagint',
-    language: 'Greek Ελληνικά',
+    key: 'dhnt',
+    versionName: 'Delitzsch\'s Hebrew New Testament 1877, 1998 (with Vowels)',
+    language: 'Hebrew עברית',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
@@ -182,19 +215,9 @@ export const BibleVersionCollection: IBibleVersion[] = [
     language: 'Hebrew עברית',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
-  {
-    key: 'dhnt',
-    versionName: 'Delitzsch\'s Hebrew New Testament 1877, 1998 (with Vowels)',
-    language: 'Hebrew עברית',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'cuv',
-    versionName: 'Chinese Union Version (Traditional)',
-    language: 'Chinese',
-    code: 'cn',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
+]
+
+export const BibleVersionCollectionChinese = [
   {
     key: 'cunp',
     versionName: 'Chinese Union New Punctuation Cross References',
@@ -210,12 +233,25 @@ export const BibleVersionCollection: IBibleVersion[] = [
     apiSource: BibleAPISourceCollection.bollsLife,
   },
   {
+    key: 'cuv',
+    versionName: 'Chinese Union Version (Traditional)',
+    language: 'Chinese',
+    code: 'cn',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+]
+
+export const BibleVersionCollectionItalian = [
+  {
     key: 'nr06',
     versionName: 'Nuova Riveduta, 2006',
     language: 'Latin / Italian',
     code: 'it',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionArabic = [
   {
     key: 'svd',
     versionName: 'Smith and Van Dyke',
@@ -223,9 +259,40 @@ export const BibleVersionCollection: IBibleVersion[] = [
     code: 'ar',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionSpanish = [
   {
     key: 'btx3',
     versionName: 'La Biblia Textual 3ra Edicion',
+    language: 'Spanish',
+    code: 'sp',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'lbla',
+    versionName: 'La Biblia de Las Americas, 1997',
+    language: 'Spanish',
+    code: 'sp',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'ntv',
+    versionName: 'Nueva Traduccion Viviente, 2009',
+    language: 'Spanish',
+    code: 'sp',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'nvi',
+    versionName: 'Nueva Version Internacional',
+    language: 'Spanish',
+    code: 'sp',
+    apiSource: BibleAPISourceCollection.bollsLife,
+  },
+  {
+    key: 'pdt',
+    versionName: 'Palabra de Dios para Todos',
     language: 'Spanish',
     code: 'sp',
     apiSource: BibleAPISourceCollection.bollsLife,
@@ -244,35 +311,10 @@ export const BibleVersionCollection: IBibleVersion[] = [
     code: 'sp',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
-  {
-    key: 'pdt',
-    versionName: 'Palabra de Dios para Todos',
-    language: 'Spanish',
-    code: 'sp',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'nvi',
-    versionName: 'Nueva Version Internacional',
-    language: 'Spanish',
-    code: 'sp',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'ntv',
-    versionName: 'Nueva Traduccion Viviente, 2009',
-    language: 'Spanish',
-    code: 'sp',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  {
-    key: 'lbla',
-    versionName: 'La Biblia de Las Americas, 1997',
-    language: 'Spanish',
-    code: 'sp',
-    apiSource: BibleAPISourceCollection.bollsLife,
-  },
-  // todo add langauge code
+]
+
+
+export const BibleVersionCollectionPolish = [
   {
     key: 'bg',
     versionName: 'Biblia gdańska, 1881',
@@ -285,30 +327,45 @@ export const BibleVersionCollection: IBibleVersion[] = [
     language: 'Polish',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionIndonesian = [
   {
     key: 'tb',
     versionName: 'Terjemahan Baru',
     language: 'Indonesian',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionRussian = [
   {
     key: 'bti',
     versionName: 'Библия под ред. М.П. Кулакова и М.М. Кулакова, 2015',
     language: 'Russian',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionUkrainian = [
   {
     key: 'utt',
     versionName: 'Українська Біблія LXX УБТ Рафаїла Турконяка (2011) 77 книг',
     language: 'Ukrainian',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionFrench = [
   {
     key: 'nbs',
     versionName: 'Nouvelle Bible Segond, 2002',
     language: 'French',
     apiSource: BibleAPISourceCollection.bollsLife,
   },
+]
+
+export const BibleVersionCollectionTamil = [
   {
     key: 'tbsi',
     versionName: 'The Bible Society of India - Tamil - O.V.',
@@ -316,6 +373,35 @@ export const BibleVersionCollection: IBibleVersion[] = [
     apiSource: BibleAPISourceCollection.bollsLife,
   },
 ]
+
+
+/**
+ * Please note to support the abbreviation of the translation
+ * "code" need to be added
+ * And the Abbreviation source need to be updated too See this repo. https://github.com/AntiochTechnology/bible-reference-toolkit/tree/master/packages/bible-book-names-intl
+ */
+export const BibleVersionCollection: IBibleVersion[] =
+  [
+    ...BibleVersionCollectionArabic,
+    ...BibleVersionCollectionCherokee,
+    ...BibleVersionCollectionChinese,
+    ...BibleVersionCollectionEnglish,
+    ...BibleVersionCollectionEnglishUK,
+    ...BibleVersionCollectionFrench,
+    ...BibleVersionCollectionGerman,
+    ...BibleVersionCollectionGreek,
+    ...BibleVersionCollectionHebrew,
+    ...BibleVersionCollectionIndonesian,
+    ...BibleVersionCollectionItalian,
+    ...BibleVersionCollectionLatin,
+    ...BibleVersionCollectionPolish,
+    ...BibleVersionCollectionPortuguese,
+    ...BibleVersionCollectionRomanian,
+    ...BibleVersionCollectionRussian,
+    ...BibleVersionCollectionSpanish,
+    ...BibleVersionCollectionTamil,
+    ...BibleVersionCollectionUkrainian,
+  ]
 
 export const DEFAULT_BIBLE_VERSION = BibleVersionCollection[11]
 
