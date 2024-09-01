b96e97ceac9ebe16137f82d13293a7a107df0b07
tim-hub <tim-hub@users.noreply.github.com>
2024-09-02 08:00:00 +1200

    fix reg

--- src/utils/regs.ts
+++ src/utils/regs.ts
@@ -4,9 +4,9 @@
  * John1:1
  * 1John1:1
  */
-export const MODAL_REG = /([123])*\s*[A-z]{2,}\s*\d{1,3}:\d{1,3}(-\d{1,3})*/
+export const MODAL_REG = /([123])*\s*[A-Z\[\\\]^_`a-z]{2,}\s*\d{1,3}:\d{1,3}(-\d{1,3})*/
 
-export const BOOK_REG = /[123]*\s*[A-z]{2,}/
+export const BOOK_REG = /[123]*\s*[A-Z\[\\\]^_`a-z]{2,}/
 
 
 /**
