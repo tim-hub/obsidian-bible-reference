908987b038366c5269dd2887cb3e467df37939a3
tim-hub <tim-hub@users.noreply.github.com>
2025-02-08 22:05:52 +1300

    disable 1. version formatting

--- src/data/BibleVerseNumberFormat.ts
+++ src/data/BibleVerseNumberFormat.ts
@@ -11,10 +11,10 @@ export enum BibleVerseNumberFormat {
 }
 
 export const BibleVerseNumberFormatCollection = [
-  {
-    name: BibleVerseNumberFormat.Period,
-    description: '1. ',
-  },
+  // {
+  //   name: BibleVerseNumberFormat.Period,
+  //   description: '1. ',
+  // },
   {
     name: BibleVerseNumberFormat.PeriodParenthesis,
     description: '1.) ',
