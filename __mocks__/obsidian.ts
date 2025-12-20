89e66456961fda4f08ea3f0053e9517fd0e8e8ad
tim-hub <tim-hub@users.noreply.github.com>
2025-12-20 18:08:08 +1300

    we don't need to test what Logos behave

--- __mocks__/obsidian.ts
+++
@@ -1,9 +0,0 @@
-// Mock for Obsidian API
-export class Notice {
-  constructor(message: string) {
-    // Mock implementation - just store the message
-    this.message = message
-  }
-
-  message: string
-}
